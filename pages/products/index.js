4bc97d582eee372ad8b1903e20eeacf392bbfaf0
Adnan A M <adnan.tlkn@gmail.com>
2021-11-30 12:28:35 +0530

    Remove commerce js for now

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,19 +1,19 @@
-import ProductList from "../../components/ProductList";
-import { client } from "../../libs/commerce";
-
-export async function getStaticProps() {
-    const { data: products } = await client.products.list();
-    return {
-        props: {
-            products,
-        },
-    };
-}
-
-export default function ProductListPage({ merchant, categories, products }) {
-    return (
-        <>
-            <ProductList products={products} />
-        </>
-    );
-}
+// import ProductList from "../../components/ProductList";
+// import { client } from "../../libs/commerce";
+//
+// export async function getStaticProps() {
+//     const { data: products } = await client.products.list();
+//     return {
+//         props: {
+//             products,
+//         },
+//     };
+// }
+//
+// export default function ProductListPage({ merchant, categories, products }) {
+//     return (
+//         <>
+//             <ProductList products={products} />
+//         </>
+//     );
+// }
